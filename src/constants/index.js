b26ca3dbeafc647878ea2576032d50a149164456
STUTI0670 <144615693+STUTI0670@users.noreply.github.com>
2025-04-21 12:39:20 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,74 +3,67 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences`;
+export const HERO_CONTENT = `A B.Tech student at IGDTUW with a deep interest in building at the intersection of hardware and software. From developing deep learning models for cryptographic stream ciphers at DRDO to launching responsive web projects using React, I love turning ideas into impactful solutions. I'm currently learning full stack development while actively mentoring peers in C++ and DSA. I've explored cybersecurity with Gurugram Police, led the IEEE web team, and designed AR-based lenses with 10K+ views. I’m passionate about AI, embedded systems, and solving real-world problems through innovative tech. Always upskilling, always building — and always excited about what comes next.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `Beyond building projects, I find purpose in creating impact through people and communities. As a C++ and DSA mentor with TechNeeds, I’ve supported over 1000 learners—resolving doubts, hosting contests, and nurturing consistent growth. I actively lead in student communities like IEEE (as Web Team Head), CodeBenders (as Technical Coordinator), and the T&P Cell (as a student representative), where I blend organization with innovation. From optimizing autoencoders at DRDO to launching socially-relevant platforms like Rakt Daan, I’ve always believed tech is most powerful when it uplifts. I also enjoy diving into music, Indian history, and French—because learning never stops.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "Jan 2025 - Present",
+    role: "Research Intern",
+    company: "DRDO",
+    description: `Worked on learning and understanding deep learning concepts and working of cryptographic stream ciphers. Implemented Autoencoders to study feature extraction and reconstruct MNIST data.`,
+    technologies: ["Neural Networks", "Ciphers", "Autoencoders"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "Jun 2024 - 2024",
+    role: "MERN Stack Trainee",
+    company: "Centre Of Excellence IGDTUW",
+    description: `Learnt and reinforced concepts of JavaScript and also learnt about various frameworks like  MongoDB, Express.js, React.js, and Node.js.`,
+    technologies: ["JavaScript", "MERN"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "Jun 2024 - Jul 2024",
+    role: "Student Trainee Intern",
+    company: "Gurugram Police",
+    description: `Participated in a 2-week internship program focused on cybersecurity, gaining insights into the latest trends and technologies in the field.`,
+    technologies: ["Cybersecurity","OSINT", "Networking"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Rakt Daan",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A web application that connects blood donors with recipients, facilitating the donation process and raising awareness about blood donation.",
+    technologies: ["HTML", "CSS","Javascript"],
   },
   {
-    title: "Task Management App",
+    title: "Snap Chat AR Lense",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "An augmented reality lens for Snapchat that allows users to interact with virtual objects in real-time.",
+    technologies: ["Lens Studio"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["React"],
   },
   {
-    title: "Blogging Platform",
+    title: "Waste Wises",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A platform for understanding different types of waste and their disposal methods, with a focus on environmental awareness. Also includes a AI chatbot for waste management queries.",
+    technologies: ["HTML", "CSS","AI-ML"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "New Delhi, India",
+  phoneNo: "+91 9953134042",
+  email: "stutistuti2@gmail.com",
 };
