ef9e7e9dd13a4726f35d8c09a73744f61f843c35
Stuti Sharma <144615693+STUTI0670@users.noreply.github.com>
2025-07-31 20:55:54 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,10 +10,16 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter/>
+   <a href="https://www.linkedin.com/in/stuti-sharma-6439281a8/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/STUTI0670" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+     
+        
+        <a href="https://twitter.com/stuts" target="_blank" rel="noopener noreferrer">
+          <FaSquareXTwitter />
       </div>
       </nav>
   )
