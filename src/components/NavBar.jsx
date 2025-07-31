acd5439cc7d48f57e3e5c875cd9a5f7845703943
Stuti Sharma <144615693+STUTI0670@users.noreply.github.com>
2025-07-31 21:09:06 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,16 +3,33 @@ import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
 import { FaSquareXTwitter } from "react-icons/fa6"
 import { FaInstagram } from "react-icons/fa"
+
 const Navbar = () => {
   return (
-      <nav className=" mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-   
+        <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-   
+        <a
+          href="https://www.linkedin.com/in/stuti-sharma-6439281a8"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/STUTI0670"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+      
       </div>
-      </nav>
+    </nav>
   )
 }
 
